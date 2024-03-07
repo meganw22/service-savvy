c4455517fa48a387827f52c8b64e84b7b177c241
Megan Walford <mwalford22@gmail.com>
2024-03-07 13:49:21 +0000

    Add a create_ticket model

--- my_requests/admin.py
+++ my_requests/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import create_ticket
 
 # Register your models here.
+admin.site.register(create_ticket)
--- my_requests/models.py
+++ my_requests/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+JOB_CATEGORY = (
+    (0, "Choose Type"),
+    (1, "Plumbing"),
+    (2, "Lighting"),
+    (3, "Heating"),
+    (4, "Cleaning"),
+    (5, "Vending Services"),
+    (6, "Air Conditioning"),
+    (7, "Waste"),
+    (8, "Other"),
+)
+
+PRIORITY = (
+    (0, "Priority Level"),
+    (1, "High (<2 hours)"),
+    (2, "Medium (<1 day)"),
+    (3, "Low (1 day +)"),
+)
 
 # Create your models here.
+class create_ticket(models.Model):
+    title = models.CharField(max_length=200)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="my_requests"
+    )
+    job_category = models.IntegerField(choices=JOB_CATEGORY, default=0)
+    job_description = models.CharField(max_length=400, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=200)
+    priority = models.IntegerField(choices=PRIORITY, default=0)
