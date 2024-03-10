d665da17a0c3616aa596a08ffd80b0149feeabe4
Megan Walford <mwalford22@gmail.com>
2024-03-10 10:42:18 +0000

    Add template structure to requests page.

--- my_requests/views.py
+++ my_requests/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.views import generic
 from .models import create_ticket
 
+# Create your views here.
 class TicketList(generic.ListView):
-    model = create_ticket
+    queryset = create_ticket.objects.all()
+    template_name = "my_requests/index.html"
+    # paginate_by = 6
