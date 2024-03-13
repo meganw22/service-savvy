4053ff72767ac6e1c0e358f54486d870c192074a
Megan Walford <mwalford22@gmail.com>
2024-03-13 20:44:32 +0000

    Amending HTML and adding style to base.html

--- tickets/views.py
+++ tickets/views.py
@@ -7,16 +7,3 @@ class TicketList(generic.ListView):
     queryset = Ticket.objects.all()
     template_name = "tickets/index.html"
     # paginate_by = 6
-
-# def post_detail(request, slug):
-#     queryset = Ticket.objects.filter(priority=1)
-#     post = get_object_or_404(queryset, slug=slug)
-
-#     return render(
-#         request,
-#         "tickets/post_detail.html",
-#         {
-#             "post": post,
-#             "coder": "Megan Walford",
-#         },
-#     )
