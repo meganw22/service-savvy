9209002912a478e9c3b6f5f99c966f6cde130334
Megan Walford <mwalford22@gmail.com>
2024-03-18 18:49:31 +0000

    Adjust views.py for tickets app

--- tickets/admin.py
+++ tickets/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Ticket, Comment, Archive
 from django_summernote.admin import SummernoteModelAdmin
 
+@admin.register(Ticket)
 class TicketAdmin(SummernoteModelAdmin):
     list_display = ('title', 'job_category', 'priority')
     search_fields = ['title']
@@ -9,6 +10,5 @@ class TicketAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Comment)
 admin.site.register(Archive)
