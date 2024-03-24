8d0b983c71b513af65e368278ffb80cf9d910ee9
Megan Walford <mwalford22@gmail.com>
2024-03-24 17:23:34 +0000

    Update about model

--- about/models.py
+++ about/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class About(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, null=True)
     full_name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
--- about/views.py
+++ about/views.py
@@ -6,7 +6,11 @@ def about_me(request):
     """
     Renders the About page
     """
-    about = About.objects.all()
+    about, created = About.objects.get_or_create(user=request.user)
+    if created:
+        # Set the user field to the currently logged-in user
+        about.user = request.user
+        about.save()
 
     return render(
         request,
