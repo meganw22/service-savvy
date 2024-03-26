243c3cf1f37b5858afc24fa166a9c970eed5980f
Megan Walford <mwalford22@gmail.com>
2024-03-26 17:01:24 +0000

    Added a filter option to tickets.html

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,7 +4,7 @@ from .views import TicketListView, TicketDetailView, CreateTicketView
 
 urlpatterns = [
     path("", TicketListView.as_view(), name="tickets"),
-    path("create/", CreateTicketView.as_view(), name="create_ticket"),
-    path("<slug:slug>/", TicketDetailView.as_view(), name="ticket_detail"),
+    path('create/', CreateTicketView.as_view(), name='create_ticket'),
+    path('<slug:slug>/', TicketDetailView.as_view(), name='ticket_detail'),
 
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,20 +1,39 @@
-from django.shortcuts import render , get_object_or_404, redirect
-from django.views.generic.edit import CreateView
-from django.views import generic
+from django.shortcuts import get_object_or_404, redirect
+from django.http import JsonResponse
+from django.views.generic import ListView, DetailView, CreateView
+from django.template.loader import render_to_string
 from .models import Ticket, JOB_CATEGORY, PRIORITY
 
 # Create your views here.
-class TicketListView(generic.ListView):
+class TicketListView(ListView):
     queryset = Ticket.objects.all()
     template_name = "tickets/tickets.html"
-    paginate_by = 5
+    paginate_by = 6
 
-class TicketDetailView(generic.DetailView):
-    queryset = Ticket.objects.all()
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        sort_by = self.request.GET.get('sort_by', 'priority')
+        
+        if sort_by == 'priority':
+            queryset = queryset.order_by('priority')
+        elif sort_by == 'username':
+            queryset = queryset.order_by('username')
+        elif sort_by == 'created_on':
+            queryset = queryset.order_by('created_on')
+        
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['selected_sort_by'] = self.request.GET.get('sort_by', 'priority')  # Default to 'priority' if not provided
+        return context
+
+class TicketDetailView(DetailView):
+    model = Ticket
     template_name = "tickets/ticket_detail.html"
 
-class CreateTicketView(generic.CreateView):
-    queryset = Ticket.objects.all()
+class CreateTicketView(CreateView):
+    model = Ticket
     template_name = 'tickets/create_ticket.html'
     fields = ['title', 'job_category', 'job_description', 'location', 'priority' ]
 
